80d4ee085d0cd110fe59d3e920865ee427db5647
jing.chen <jing.chen@getinsured.com>
2016-11-17 22:43:39 -0800

    Add code for concurrent.

--- learn-you-some-erlang/dolphins.erl
+++ learn-you-some-erlang/dolphins.erl
@@ -2,33 +2,77 @@
 -compile(export_all).
 
 dolphin1() ->
-    receive
-        do_a_flip ->
-            io:format("How about no?~n");
-        fish ->
-            io:format("So long and thanks for all the fish!~n");
-        _ ->
-            io:format("Heh, we're smarter than you humans.~n")
-    end.
+  receive
+    do_a_flip ->
+      io:format("How about no?~n");
+    fish ->
+      io:format("So long and thanks for all the fish!~n");
+    _ ->
+      io:format("Heh, we're smarter than you humans.~n")
+  end.
 
 dolphin2() ->
-    receive
-        {From, do_a_flip} ->
-            From ! "How about no?";
-        {From, fish} ->
-            From ! "So long and thanks for all the fish!";
-        _ ->
-            io:format("Heh, we're smarter than you humans.~n")
-    end.
+  receive
+    {From, do_a_flip} ->
+      From ! "How about no?";
+    {From, fish} ->
+      From ! "So long and thanks for all the fish!";
+    _ ->
+      io:format("Heh, we're smarter than you humans.~n")
+  end.
 
 dolphin3() ->
-    receive
-        {From, do_a_flip} ->
-            From ! "How about no?",
-            dolphin3();
-        {From, fish} ->
-            From ! "So long and thanks for all the fish!";
-        _ ->
-            io:format("Heh, we're smarter than you humans.~n"),
-            dolphin3()
-    end.
+  receive
+    {From, do_a_flip} ->
+      From ! "How about no?",
+      dolphin3();
+    {From, fish} ->
+      From ! "So long and thanks for all the fish!";
+    _ ->
+      io:format("Heh, we're smarter than you humans.~n"),
+      dolphin3()
+  end.
+
+
+%%Test:
+%%14> c(dolphins).
+%%{ok,dolphins}
+%%15> Dolphin = spawn(dolphins, dolphin1, []).
+%%<0.83.0>
+%%16> Dolphin ! "oh, hello dolphin!".
+%%Heh, we're smarter than you humans.
+%%"oh, hello dolphin!"
+%%17> Dolphin ! fish
+%%fish
+%%18>
+%%18> f(Dolphin).
+%%ok
+%%19> Dolphin = spawn(dolphins, dolphin1, []).
+%%<0.89.0>
+%%20> Dolphin ! fish.
+%%So long and thanks for all the fish!
+%%fish
+%%21>
+%%21> Dolphin2 = spawn(dolphins, dolphin2, []).
+%%<0.92.0>
+%%22> Dolphin2 ! {self(), do_a_flip}.
+%%{<0.57.0>,do_a_flip}
+%%23> flush().
+%%Shell got "How about no?"
+%%ok
+%%24> Dolphin3 = spawn(dolphins, dolphin3, []).
+%%<0.96.0>
+%%25> Dolphin3 ! Dolphin3 ! {self(), do_a_flip}.
+%%{<0.57.0>,do_a_flip}
+%%26> flush().
+%%Shell got "How about no?"
+%%Shell got "How about no?"
+%%ok
+%%27> Dolphin3 ! {self(), unknown_message}.
+%%Heh, we're smarter than you humans.
+%%{<0.57.0>,unknown_message}
+%%29> Dolphin3 ! Dolphin3 ! {self(), fish}.
+%%{<0.57.0>,fish}
+%%30> flush().
+%%Shell got "So long and thanks for all the fish!"
+%%ok
--- learn-you-some-erlang/kitchen.erl
+++ learn-you-some-erlang/kitchen.erl
@@ -2,63 +2,101 @@
 -compile(export_all).
 
 start(FoodList) ->
-    spawn(?MODULE, fridge2, [FoodList]).
+  spawn(?MODULE, fridge2, [FoodList]).
 
 store(Pid, Food) ->
-    Pid ! {self(), {store, Food}},
-    receive
-        {Pid, Msg} -> Msg
-    end.
+  Pid ! {self(), {store, Food}},
+  receive
+    {Pid, Msg} -> Msg
+  end.
 
 take(Pid, Food) ->
-    Pid ! {self(), {take, Food}},
-    receive
-        {Pid, Msg} -> Msg
-    end.
+  Pid ! {self(), {take, Food}},
+  receive
+    {Pid, Msg} -> Msg
+  end.
 
 store2(Pid, Food) ->
-    Pid ! {self(), {store, Food}},
-    receive
-        {Pid, Msg} -> Msg
-    after 3000 ->
-        timeout
-    end.
+  Pid ! {self(), {store, Food}},
+  receive
+    {Pid, Msg} -> Msg
+  after 3000 ->
+    timeout
+  end.
 
 take2(Pid, Food) ->
-    Pid ! {self(), {take, Food}},
-    receive
-        {Pid, Msg} -> Msg
-    after 3000 ->
-        timeout
-    end.
+  Pid ! {self(), {take, Food}},
+  receive
+    {Pid, Msg} -> Msg
+  after 3000 ->
+    timeout
+  end.
 
 fridge1() ->
-    receive
-        {From, {store, _Food}} ->
-            From ! {self(), ok},
-            fridge1();
-        {From, {take, _Food}} ->
-            %% uh....
-            From ! {self(), not_found},
-            fridge1();
-        terminate ->
-            ok
-    end.
+  receive
+    {From, {store, _Food}} ->
+      From ! {self(), ok},
+      fridge1();
+    {From, {take, _Food}} ->
+      %% uh....
+      From ! {self(), not_found},
+      fridge1();
+    terminate ->
+      ok
+  end.
 
 fridge2(FoodList) ->
-    receive
-        {From, {store, Food}} ->
-            From ! {self(), ok},
-            fridge2([Food|FoodList]);
-        {From, {take, Food}} ->
-            case lists:member(Food, FoodList) of
-                true ->
-                    From ! {self(), {ok, Food}},
-                    fridge2(lists:delete(Food, FoodList));
-                false ->
-                    From ! {self(), not_found},
-                    fridge2(FoodList)
-            end;
-        terminate ->
-            ok
-    end.
+  receive
+    {From, {store, Food}} ->
+      From ! {self(), ok},
+      fridge2([Food | FoodList]);
+    {From, {take, Food}} ->
+      case lists:member(Food, FoodList) of
+        true ->
+          From ! {self(), {ok, Food}},
+          fridge2(lists:delete(Food, FoodList));
+        false ->
+          From ! {self(), not_found},
+          fridge2(FoodList)
+      end;
+    terminate ->
+      ok
+  end.
+
+
+%%Test:
+%%1> c(kitchen).
+%%{ok,kitchen}
+%%2> Pid = spawn(kitchen, fridge2, [[baking_soda]]).
+%%<0.64.0>
+%%3> flush().
+%%ok
+%%4> Pid ! {self(), {store, milk}}.
+%%{<0.57.0>,{store,milk}}
+%%5> flush().
+%%Shell got {<0.64.0>,ok}
+%%ok
+%%6> Pid ! {self(), {store, bacon}}.
+%%{<0.57.0>,{store,bacon}}
+%%7> flush().
+%%Shell got {<0.64.0>,ok}
+%%ok
+%%8> Pid ! {self(), {take, bacon}}.
+%%{<0.57.0>,{take,bacon}}
+%%9> Pid ! {self(), {take, turkey}}.
+%%{<0.57.0>,{take,turkey}}
+%%10>
+%%10> flush().
+%%Shell got {<0.64.0>,{ok,bacon}}
+%%Shell got {<0.64.0>,not_found}
+%%ok
+%%11> Pid ! {self(), {take, baking_soda}}.
+%%{<0.57.0>,{take,baking_soda}}
+%%12> flush().
+%%Shell got {<0.64.0>,{ok,baking_soda}}
+%%ok
+%%13> Pid ! {self(), {take, purified_water}}.
+%%{<0.57.0>,{take,purified_water}}
+%%14> flush().
+%%Shell got {<0.64.0>,not_found}
+%%ok
